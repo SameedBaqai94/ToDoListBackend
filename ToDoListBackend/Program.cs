ca0a21f1b71aa9233d5d6a843a3547be6018d9de
sameedbaqai94 <sameedbaqai94@gmail.com>
2024-04-07 03:25:07 -0400

    done

--- ToDoListBackend/Program.cs
+++ ToDoListBackend/Program.cs
@@ -19,8 +19,10 @@ namespace ToDoListBackend
                                 policy =>
                                 {
                                     policy.WithOrigins("http://127.0.0.1:5500")
-                                    .AllowAnyHeader()
-                                    .AllowAnyMethod();
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod()
+                                        .SetIsOriginAllowed(origin => true)
+                                        .AllowCredentials();
                                 });
             });
 
--- ToDoListBackend/obj/Debug/net8.0/ToDoListBackend.AssemblyInfo.cs
+++ ToDoListBackend/obj/Debug/net8.0/ToDoListBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a1818d1401e6b7af830a1d8e31ab348b8283e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165a1a310d34e5f462e73edd5cea83215497e730")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
